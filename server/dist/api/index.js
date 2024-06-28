b05cfd8e0421d70009d575b470ad549ef929177e
Walter Clayton <walter.h.clayton@gmail.com>
2024-06-28 23:35:21 +0100

    added vercel route changed rewrites

--- server/dist/api/index.js
+++ server/dist/api/index.js
@@ -8,46 +8,29 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const rpe_routes_1 = __importDefault(require("../routes/rpe.routes"));
-const path = require("path");
 require("./database");
-
 const app = (0, express_1.default)();
 dotenv.config();
-
 const corsOptions = {
     origin: [
+        "http://localhost:5174",
         "http://localhost:5173",
         "https://www.afitpilot.com/",
-        "https://rpe-scoring.vercel.app",
-        "https://rpe-scoring-front-github.vercel.app",
+        "https://rpe-scoring-front.vercel.app",
+        process.env.FRONTEND_URL,
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     optionsSuccessStatus: 200,
 };
-
 app.use(cors(corsOptions));
-
 const PORT = parseInt(process.env.PORT) || 4000;
 app.set("port", PORT);
-
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express_1.default.json({ limit: "50mb" }));
 app.use(express_1.default.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
-
-// API routes
 app.use("/api", rpe_routes_1.default);
-
-// Serve static files from the React app
-app.use(express_1.default.static(path.join(__dirname, '../client/build')));
-
-// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-    console.log('Request received for:', req.url); // Add this line for debugging
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-  });
-
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
 });
