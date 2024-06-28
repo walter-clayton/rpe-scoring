354a6bd2541dc302b89f0e25bee9e42ba08be209
Walter Clayton <walter.h.clayton@gmail.com>
2024-06-28 23:27:51 +0100

    added routes to vercel

--- server/dist/api/index.js
+++ server/dist/api/index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const rpe_routes_1 = __importDefault(require("../routes/rpe.routes"));
-const path = require("path"); // Import path module
+const path = require("path");
 require("./database");
 
 const app = (0, express_1.default)();
@@ -44,8 +44,9 @@ app.use(express_1.default.static(path.join(__dirname, '../client/build')));
 
 // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
+    console.log('Request received for:', req.url); // Add this line for debugging
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
