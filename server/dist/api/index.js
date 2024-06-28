d5054941cb45f886db965bfe11bf2284e8c40451
Walter Clayton <walter.h.clayton@gmail.com>
2024-06-28 23:12:39 +0100

    added catch all route for page refresh

--- server/dist/api/index.js
+++ server/dist/api/index.js
@@ -8,9 +8,12 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const rpe_routes_1 = __importDefault(require("../routes/rpe.routes"));
+const path = require("path");
 require("./database");
+
 const app = (0, express_1.default)();
 dotenv.config();
+
 const corsOptions = {
     origin: [
         "http://localhost:5173",
@@ -22,14 +25,27 @@ const corsOptions = {
     credentials: true,
     optionsSuccessStatus: 200,
 };
+
 app.use(cors(corsOptions));
+
 const PORT = parseInt(process.env.PORT) || 4000;
 app.set("port", PORT);
+
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express_1.default.json({ limit: "50mb" }));
 app.use(express_1.default.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
+
 app.use("/api", rpe_routes_1.default);
+
+// Serve static files from the React app
+app.use(express_1.default.static(path.join(__dirname, '../client/build')));
+
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
 });
